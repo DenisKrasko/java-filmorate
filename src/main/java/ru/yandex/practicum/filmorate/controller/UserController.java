c91dc6825b6340580a9a73494527a699e0f1053e
Denis Krasko <iidonz143@gmail.com>
2026-06-18 04:41:17 +0300

    First commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,111 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	private final Map<Long, User> users = new HashMap<>();
+	private final static Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@GetMapping
+	public Collection<User> findAll() {
+		return users.values();
+	}
+
+	@PostMapping
+	public User create(@RequestBody User user) {
+		log.trace("проверка выполнения необходимых условий при эндпоинте Post");
+		validateUser(user);
+		log.trace("формирование дополнительных данных");
+		user.setId(getNextId());
+		log.trace("сохранение новой публикации в памяти приложения");
+		users.put(user.getId(), user);
+		return user;
+	}
+
+	@PutMapping
+	public User update(@RequestBody User newUser) {
+		log.trace("проверка выполнения необходимых условий при эндпоинте Put");
+		validateUser(newUser);
+		if (newUser.getId() == null) {
+			logAndThrow("Id должен быть указан");
+		}
+		if (users.containsKey(newUser.getId())) {
+			if (!users.get(newUser.getId()).getEmail().equals(newUser.getEmail())) {
+				for (User user1 : users.values()) {
+					if (user1.getEmail().equals(newUser.getEmail())) {
+						throw new DuplicatedDataException("Этот имейл уже используется");
+					}
+				}
+			}
+			User oldUser = users.get(newUser.getId());
+			log.trace("пользователь найден и все условия соблюдены, обновляем его содержимое");
+			oldUser.setLogin(newUser.getLogin());
+			oldUser.setEmail(newUser.getEmail());
+			if (oldUser.getName().isBlank()) {
+				oldUser.setName(oldUser.getLogin());
+			} else {
+				oldUser.setName(newUser.getName());
+			}
+			oldUser.setBirthday(newUser.getBirthday());
+			return oldUser;
+		}
+		log.error("User с id = {} не найден", newUser.getId());
+		throw new NotFoundException("User с id = " + newUser.getId() + " не найден");
+	}
+
+	/** Вспомогательный метод для генерации идентификатора нового поста */
+	private long getNextId() {
+		long currentMaxId = users.keySet()
+				.stream()
+				.mapToLong(id -> id)
+				.max()
+				.orElse(0);
+		return ++currentMaxId;
+	}
+
+	private void logAndThrow(String message) {
+		log.error(message);
+		throw new ConditionsNotMetException(message);
+	}
+
+	private void validateUser(User user) {
+		if (user.getEmail() == null || user.getEmail().isBlank()) {
+			logAndThrow("Имейл должен быть указан");
+		}
+		if (user.getEmail().charAt(0) != '@') {
+			logAndThrow("электронная почта должна содержать символ @");
+		}
+		if (user.getLogin() == null || user.getLogin().isBlank()) {
+			logAndThrow("логин не может быть пустым");
+		}
+		if (user.getLogin().contains(" ")) {
+			logAndThrow("логин не может содержать пробелы");
+		}
+		if (user.getBirthday().isAfter(LocalDate.now())){
+			logAndThrow("дата рождения не может быть в будущем");
+		}
+		for (User user1 : users.values()) {
+			if (user1.getEmail().equals(user.getEmail())) {
+				String msg = "электронная почта должна содержать символ @";
+				log.error(msg);
+				throw new DuplicatedDataException(msg);
+			}
+		}
+		if (user.getName().isBlank()) {
+			user.setName(user.getLogin());
+		}
+	}
+}
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -3,8 +3,16 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.http.HttpClient;
+
 @SpringBootTest
 class FilmorateApplicationTests {
+	private static final String BASE = "http://localhost:8080/movies";
+	private static final String JSON_CONTENT_TYPE = "application/json; charset=UTF-8";
+//	private static MoviesServer server;
+//	private static HttpClient client;
+//	private static MoviesStore moviesStore = new MoviesStore();
+//	private Gson gson = new Gson();
 
 	@Test
 	void contextLoads() {
